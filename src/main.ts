0dc7e3c0933b74fb6e3640036b70bacce0e42fa9
parinya-06 <parinya.ru.61@ubu.ac.th>
2023-10-03 14:21:01 +0700

    add swagger in project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Test API MSSQL example')
+    .setDescription('The MSSQL API description')
+    .setVersion('0.1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
   app.enableCors();
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
   console.log(`API is running on: ${await app.getUrl()}`);
 }
